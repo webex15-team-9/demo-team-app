a2b76b03dc71b9ec6f80335e62882727db7923ff
石井悠人” <yuto120925@outlook.jp>
2022-08-16 22:27:40 +0900

    palatte

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,8 @@
 <template>
   <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <div>
+    <a href="quiz">Vue クイズ</a><br />
+    <a href="palette">Vue パレット</a><br />
+    <a href="memo">Vue メモ</a><br />
+  </div>
 </template>
