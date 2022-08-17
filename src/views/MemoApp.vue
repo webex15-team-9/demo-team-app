21229751ee6a79320734b8d945b1852de6871253
ChihiroMorimoto <k10fwnjsk7867.wfty@gmail.com>
2022-08-17 15:16:52 +0900

    一応できた

--- src/views/MemoApp.vue
+++ src/views/MemoApp.vue
@@ -3,12 +3,15 @@
   <div class="memo-list">
     <ul class="memo-list_container">
       <li v-for="(memo, index) in memos" v-bind:key="index" class="memo">
+        <div class="memo_checkbox">
+          <input type="checkbox" />
+        </div>
         <div class="memo_text">{{ memo.content }}</div>
         <button class="memo_delete" v-on:click="deleteMemo(index)">削除</button>
       </li>
     </ul>
     <div class="add-memo-field">
-      <input type="text" v-model="inputMemo" />
+      <input class="add-memo-field_input" type="text" v-model="inputMemo" />
       <button class="add-memo-field_button" v-on:click="addMemo">追加</button>
     </div>
   </div>
@@ -49,7 +52,7 @@ export default {
   margin-right: auto;
 }
 
-.memo-list__container {
+.memo-list_container {
   padding: 0;
 }
 
@@ -66,16 +69,12 @@ export default {
   background-color: #b23b61;
 }
 
-.memo__text {
+.memo_text {
   margin-left: 2rem;
   text-align: left;
 }
 
-.memo__text--done {
-  text-decoration-line: line-through;
-}
-
-.memo__delete {
+.memo_delete {
   margin-left: 1rem;
   padding: 0.5rem 0.5rem;
   border: solid 1px black;
@@ -83,7 +82,7 @@ export default {
   background-color: white;
 }
 
-.memo__delete:hover {
+.memo_delete:hover {
   background-color: #b2ae3b;
   border-radius: 5px;
 }
@@ -94,17 +93,20 @@ export default {
   gap: 0.5rem;
 }
 
-.add-memo-field__input {
-  padding: 10px;
+.add-memo-field_input {
+  height: 5px;
+  padding: 20px;
+  font-size: 15px;
 }
-.add-memo-field__button {
+
+.add-memo-field_button {
   padding: 0.5rem 0.5rem;
   border: solid 1px black;
   border-radius: 5px;
   background-color: white;
 }
 
-.add-memo-field__button:hover {
+.add-memo-field_button:hover {
   background-color: #b2ae3b;
   border-radius: 5px;
 }
