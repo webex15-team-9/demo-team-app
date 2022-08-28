d13f8159d3e317118e923e9b579e55db8219513f
ChihiroMorimoto <k10fwnjsk7867.wfty@gmail.com>
2022-08-28 14:39:24 +0900

    少し手直し

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div>
       This Application is made by
-      <span class="names">森本千尋</span>.
+      <span class="names">仲佳澄,石井悠人,森本千尋</span>.
     </div>
     <small
       >The Application is created as a part of the GeekSalon WebExpert
